e17f9106cccfbe102638e4dcc8c05263cc94d44f
Datzu712 <devdatzucl@gmail.com>
2025-12-11 18:27:35 -0600

    feat: added DataFetcher class for handling AJAX request with retries, state managment and more

--- ProyectoProgramacionAvanzada/Scripts/main.js
+++ ProyectoProgramacionAvanzada/Scripts/main.js
@@ -1,5 +1,23 @@
 ﻿import { ToastManager } from './modules/toast.js';
+import { DataFetcher } from './modules/api.js';
 
 window.App = {
     toast: ToastManager.getInstance(document.getElementById('toast-container')),
-} 
+    _testFetcher: new DataFetcher({ userId: 1, someCrazyFilters: {} }, {
+        queryFn: async (state) => {
+            console.log('state', state);
+            await new Promise((r) => setTimeout(r, 1000));
+            throw new Error('Simulated fetch error');
+            
+            return {
+                data: `Fetched data for state: ${JSON.stringify(state)}`
+            };
+        },
+        onSuccess: (res) => {
+            console.log('Fetch successful:', res);
+        },
+        onError: (err) => {
+            console.error('Fetch error:', err);
+        },
+    })
+}
